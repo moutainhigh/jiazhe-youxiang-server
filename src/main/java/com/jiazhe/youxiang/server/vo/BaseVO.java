eaea77809a7371742d5f0e4c64b54de4e786cb14
聂箫 <niexiao@maoyan.com>
2018-10-12 14:42:01 +0800

    添加切面，记录访问日志

---
+++ src/main/java/com/jiazhe/youxiang/server/vo/BaseVO.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017 maoyan.com
+ * All rights reserved.
+ *
+ */
+package com.jiazhe.youxiang.server.vo;
+
+/**
+ * 在这里编写类的功能描述
+ *
+ * @author niexiao
+ * @created 2018/9/30
+ */
+public class BaseVO extends BaseObject {
+
+    private static final long serialVersionUID = 4745731012625923452L;
+}
