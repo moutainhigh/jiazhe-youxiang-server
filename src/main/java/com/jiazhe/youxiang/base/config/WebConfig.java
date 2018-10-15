70cf97b884dc7cf2be83af08327df4e103528e39
聂箫 <niexiao@maoyan.com>
2018-10-15 11:58:53 +0800

    优化swagger细节

---
+++ src/main/java/com/jiazhe/youxiang/base/config/WebConfig.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2017 maoyan.com
+ * All rights reserved.
+ *
+ */
+package com.jiazhe.youxiang.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 在这里编写类的功能描述
+ *
+ * @author niexiao
+ * @created 2018/10/15
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
+        // 解决 SWAGGER 404报错
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    
+}
