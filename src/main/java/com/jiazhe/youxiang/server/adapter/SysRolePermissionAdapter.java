286e703c90a40bb9d43eef2cc7d84deeb8023e4e
giserTu <961442649@qq.com>
2018-10-18 22:57:35 +0800

    调整完分层，具体VO DTO PO 里成员变量待调整，Adapter的内容也待调整

---
+++ src/main/java/com/jiazhe/youxiang/server/adapter/SysRolePermissionAdapter.java
@@ -0,0 +1,7 @@
+package com.jiazhe.youxiang.server.adapter;
+
+/**
+ * Created by tujia on 2018/10/18.
+ */
+public class SysRolePermissionAdapter {
+}
---
+++ src/main/java/com/jiazhe/youxiang/server/dto/sysrole/SysRolePermissionDTO.java
@@ -0,0 +1,7 @@
+package com.jiazhe.youxiang.server.dto.sysrole;
+
+/**
+ * Created by tujia on 2018/10/18.
+ */
+public class SysRolePermissionDTO {
+}
