6b8c278f643b25fe8ef8f64dd7e8f31c96596909
聂箫 <niexiao@maoyan.com>
2018-11-14 21:36:41 +0800

    微调

--- src/main/java/com/jiazhe/youxiang/base/controller/BaseController.java
+++ src/main/java/com/jiazhe/youxiang/base/controller/BaseController.java
@@ -20,13 +20,13 @@ public abstract class BaseController {
     /**
      * 权限异常
      */
-    @ExceptionHandler({ UnauthorizedException.class, AuthorizationException.class })
+    @ExceptionHandler({UnauthorizedException.class, AuthorizationException.class})
     public String authorizationException(HttpServletRequest request, HttpServletResponse response) throws IOException {
         if (ProjectUtil.isAjax(request)) {
             String code = "000002";
             String msg = "无权限";
             System.out.println("BaseController：ajax请求，无权限");
-            ResponseUtil.responseUtils(response, ResultPackage.resultPackage(code,new JSONObject(),msg));
+            ResponseUtil.responseUtils(response, ResultPackage.resultPackage(code, new JSONObject(), msg));
             return null;
         } else {
             System.out.println("BaseController：页面跳转，无权限");
