6bedb0a176ed54973193c6ca97a45d23fbb7fa95
giserTu <961442649@qq.com>
2018-11-26 20:47:45 +0800

    前台直接截取字符串进行日期或时间的展示

--- src/main/resources/static/common.js
+++ src/main/resources/static/common.js
@@ -33,22 +33,24 @@ function getCookie(key) {
 }
 
 function timeFormat(time) {
-    var d = new Date(time);
-    var year = d.getFullYear();
-    var month = (d.getMonth() + 1) > 9 ? (d.getMonth() + 1) : '0' + (d.getMonth());
-    var date = d.getDate() > 9 ? d.getDate() : '0' + d.getDate();
-    var hours = d.getHours() > 9 ? d.getHours() : '0' + d.getHours();
-    var minutes = d.getMinutes() > 9 ? d.getMinutes() : '0' + d.getMinutes();
-    var seconds = d.getSeconds() > 9 ? d.getSeconds() : '0' + d.getSeconds();
-    return year + '-' + month + '-' + date + ' ' + hours + ':' + minutes + ':' + seconds;
+    return time.replace('T',' ').substring(0,19);
+    // var d = new Date(time);
+    // var year = d.getFullYear();
+    // var month = (d.getMonth() + 1) > 9 ? (d.getMonth() + 1) : '0' + (d.getMonth());
+    // var date = d.getDate() > 9 ? d.getDate() : '0' + d.getDate();
+    // var hours = d.getHours() > 9 ? d.getHours() : '0' + d.getHours();
+    // var minutes = d.getMinutes() > 9 ? d.getMinutes() : '0' + d.getMinutes();
+    // var seconds = d.getSeconds() > 9 ? d.getSeconds() : '0' + d.getSeconds();
+    // return year + '-' + month + '-' + date + ' ' + hours + ':' + minutes + ':' + seconds;
 }
 
 function dateFormat(time) {
-    var d = new Date(time);
-    var year = d.getFullYear();
-    var month = (d.getMonth() + 1) > 9 ? (d.getMonth() + 1) : '0' + (d.getMonth());
-    var date = d.getDate() > 9 ? d.getDate() : '0' + d.getDate();
-    return year + '-' + month + '-' + date;
+    return time.substring(0,10);
+    // var d = new Date(time);
+    // var year = d.getFullYear();
+    // var month = (d.getMonth() + 1) > 9 ? (d.getMonth() + 1) : '0' + (d.getMonth());
+    // var date = d.getDate() > 9 ? d.getDate() : '0' + d.getDate();
+    // return year + '-' + month + '-' + date;
 }
 
 /**
