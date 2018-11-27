683f553226c549deb3e33c43238a0dffc7494e33
聂箫 <niexiao@maoyan.com>
2018-11-27 16:16:16 +0800

    清理TODO

---
+++ src/main/java/com/jiazhe/youxiang/server/common/enums/UploadCodeEnum.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017 maoyan.com
+ * All rights reserved.
+ *
+ */
+package com.jiazhe.youxiang.server.common.enums;
+
+/**
+ * 在这里编写类的功能描述
+ *
+ * @author niexiao
+ * @created 2018/11/27
+ */
+public class UploadCodeEnum {
+}
---
+++ src/main/java/com/jiazhe/youxiang/server/common/exceptions/UploadException.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2017 maoyan.com
+ * All rights reserved.
+ *
+ */
+package com.jiazhe.youxiang.server.common.exceptions;
+
+import com.jiazhe.youxiang.server.common.enums.CommonCodeEnum;
+import com.jiazhe.youxiang.server.common.enums.ProductCodeEnum;
+import com.jiazhe.youxiang.server.vo.ResponseMsg;
+
+/**
+ * 在这里编写类的功能描述
+ *
+ * @author niexiao
+ * @created 2018/10/30
+ */
+public class ProductException extends CommonException {
+
+    public ProductException(ProductCodeEnum productCodeEnum) {
+        super(productCodeEnum.getCode(), productCodeEnum.getType(), productCodeEnum.getMessage());
+    }
+}
