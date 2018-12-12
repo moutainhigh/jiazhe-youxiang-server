fc4134ffeb69c9e2b9d43fdd53a049f2257d6cbc
聂箫 <niexiao@maoyan.com>
2018-12-12 11:58:58 +0800

    添加部署脚本

---
+++ src/main/resources/deploy/deploy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+APP_DIR=/opt/jiazhe/webserver
+APP_NAME=youxiang-server
+
+start(){
+  checkpid
+  if [ ! -n "$pid" ]; then
+    nohup java -jar $APP_DIR/"$APP_NAME".jar > /dev/null 2>&1 &
+    echo "---------------------------------"
+    echo "启动完成，按CTRL+C退出日志界面即可>>>>>"
+    echo "---------------------------------"
+  else
+      echo "$APP_NAME is runing PID: $pid"
+  fi
+
+}
+
+
+status(){
+   checkpid
+   if [ ! -n "$pid" ]; then
+     echo "$APP_NAME not runing"
+   else
+     echo "$APP_NAME runing PID: $pid"
+   fi
+}
+
+checkpid(){
+    pid=`ps -ef |grep $JAR_FILE |grep -v grep |awk '{print $2}'`
+}
+
+stop(){
+    checkpid
+    if [ ! -n "$pid" ]; then
+     echo "$APP_NAME not runing"
+    else
+      echo "$APP_NAME stop..."
+      kill -9 $pid
+    fi
+}
+
+restart(){
+    stop
+    sleep 1s
+    start
+}
+
+case $1 in
+          start) start;;
+          stop)  stop;;
+          restart)  restart;;
+          status)  status;;
+              *)  echo "require start|stop|restart|status"  ;;
+esac
