b4c9053fdb1b0f2a6f4cabe14f7274c1ed3d1091
聂箫 <niexiao@maoyan.com>
2018-12-13 10:59:30 +0800

    部署文件第一版

--- src/main/resources/deploy/run.sh
+++ src/main/resources/deploy/run.sh
@@ -1,82 +1,61 @@
-#!/usr/bin/env bash
-#
-#
+#!/bin/bash
+
 APP_DIR=/opt/jiazhe/webserver
 APP_NAME=youxiang-server
-APP_CONF=$APP_DIR/application.properties
-
-#set java home
-#export JAVA_HOME=/opt/jdk1.8.0_111
-
-usage() {
-    echo "Usage: sh run.sh [start|stop|deploy]"
-    exit 1
-}
-
-kills(){
-    tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
-    if [[ $tpid ]]; then
-        echo 'Kill Process!'
-        kill -9 $tpid
-    fi
-}
+JAR_FILE=$APP_NAME.jar
+pid=0
 
 start(){
-    rm -f $APP_DIR/tpid
-    #nohup java -jar myapp.jar --spring.config.location=application.yml > /dev/null 2>&1 &
-    nohup java -jar $APP_DIR/"$APP_NAME".jar > /dev/null 2>&1 &
-    echo $! > $APP_DIR/tpid
+  checkpid
+  if [ ! -n "$pid" ]; then
+    echo "$APP_NAME start..."
+    cd $APP_DIR
+    JAVA_CMD="nohup java -jar $APP_DIR/$APP_NAME.jar > /dev/null 2>&1 &"
+    $JAVA_CMD
     echo Start Success!
+  else
+      echo "$APP_NAME is runing PID: $pid"
+  fi
+
+}
+
+
+status(){
+   checkpid
+   if [ ! -n "$pid" ]; then
+     echo "$APP_NAME not runing"
+   else
+     echo "$APP_NAME runing PID: $pid"
+   fi
+}
+
+checkpid(){
+    echo "$APP_NAME checkpid..."
+    pid=`ps -ef |grep $JAR_FILE |grep -v grep |awk '{print $2}'`
+    echo "$APP_NAME pid=$pid"
 }
 
 stop(){
-        tpid1=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
-    echo tpid1-$tpid1
-        if [[ $tpid1 ]]; then
-        echo 'Stop Process...'
-        kill -15 $tpid1
-    fi
-    sleep 5
-    tpid2=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
-        echo tpid2-$tpid2
-    if [[ $tpid2 ]]; then
-        echo 'Kill Process!'
-        kill -9 $tpid2
-    else
-        echo 'Stop Success!'
-    fi
-
-}
-
-check(){
-    tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
-    if [[ tpid ]]; then
-        echo 'App is running.'
-    else
-        echo 'App is NOT running.'
+    checkpid
+    if [ ! -n "$pid" ]; then
+     echo "$APP_NAME not runing"
+    els
+      echo "$APP_NAME stop..."
+      kill -9 $pid
     fi
 }
 
-deploy() {
-    kills
-    rm -rf $APP_DIR/"$APPNAME".jar
-    cp "$APP_NAME".jar $APP_DIR
+restart(){
+    stop
+    sleep 1s
+    start
 }
 
-case "$1" in
-    "start")
-        start
-        ;;
-    "stop")
-        stop
-        ;;
-    "kill")
-        kills
-        ;;
-    "deploy")
-        deploy
-        ;;
-    *)
-        usage
-        ;;
+
+case $1 in
+          start) start;;
+          stop)  stop;;
+          restart)  restart;;
+          status)  status;;
+              *)  echo "require start|stop|restart|status"  ;;
 esac
