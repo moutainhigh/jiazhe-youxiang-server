93e71a5915f56e8ff8018eb678a073c84d3ee72f
giserTu <961442649@qq.com>
2019-03-04 10:15:50 +0800

    sql调整

--- src/main/resources/document/alter/3.1.sql
+++ src/main/resources/document/alter/3.1.sql
@@ -1,7 +1,7 @@
 /**
  商家订单模块
  */
-alter table partner_order_info modify column status TINYINT COMMENT '订单状态 1待派单,2待服务,3已完成,4已取消';
+alter table partner_order_info modify column status TINYINT(4) NOT NULL DEFAULT '1' COMMENT '订单状态 1待派单,2待服务,3已完成,4已取消';
 update partner_order_info set status=status+1;
 
 /**
@@ -13,12 +13,12 @@ alter table audit_record add column charge_receipt_status TINYINT(4) NOT NULL DE
 alter table audit_record add column point_codes VARCHAR(1023) NOT NULL DEFAULT '' COMMENT '当兑换类型为2时，记录积分兑换码的卡号';
 alter table audit_record add column product_value DECIMAL(8 , 2 ) NOT NULL DEFAULT '0.00' COMMENT '当兑换类型为3时，记录所给实物的价值';
 
-alter table audit_record modify column point_ids VARCHAR(1023) COMMENT '当兑换类型为1时，记录积分卡id集合';
+alter table audit_record modify column point_ids VARCHAR(1023) NOT NULL DEFAULT '' COMMENT '当兑换类型为1时，记录积分卡id集合';
 alter table audit_record modify column audit_reason VARCHAR(100) COMMENT '驳回理由';
-alter table audit_record modify column remark VARCHAR(1023) COMMENT '提交人填写备注信息';
-alter table audit_record modify column exchange_point DECIMAL(8 , 2 ) COMMENT '总兑换积分';
+alter table audit_record modify column remark VARCHAR(1023) NOT NULL DEFAULT '' COMMENT '提交人填写备注信息';
+alter table audit_record modify column exchange_point DECIMAL(8 , 2 ) NOT NULL DEFAULT '0.00' COMMENT '总兑换积分';
 
-alter table audit_record modify column status  TINYINT COMMENT '审核状态 1-已保存，2-已提交，3-已驳回，4-已通过';
+alter table audit_record modify column status  TINYINT(4) NOT NULL DEFAULT '1' COMMENT '审核状态 1-已保存，2-已提交，3-已驳回，4-已通过';
 update audit_record set status=status+2;
 
 alter table audit_record add column temp_col VARCHAR(100);
@@ -45,3 +45,16 @@ CREATE TABLE `charge_receipt` (
   `mod_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`)
 )  ENGINE=INNODB COMMENT='充值凭证表';
+
+alter table point_exchange_code_batch modify column validity_period TINYINT(10) NOT NULL DEFAULT '0' COMMENT '自兑换、激活之日起多少天有效';
+alter table point_exchange_code modify column validity_period TINYINT(10) NOT NULL DEFAULT '0' COMMENT '自兑换、激活之日起多少天有效';
+alter table recharge_card_exchange_code_batch modify column validity_period TINYINT(10) NOT NULL DEFAULT '0' COMMENT '自兑换、激活之日起多少天有效';
+alter table recharge_card_exchange_code modify column validity_period TINYINT(10) NOT NULL DEFAULT '0' COMMENT '自兑换、激活之日起多少天有效';
+alter table voucher_exchange_code_batch modify column validity_period TINYINT(10) NOT NULL DEFAULT '0' COMMENT '自兑换、激活之日起多少天有效';
+alter table voucher_exchange_code modify column validity_period TINYINT(10) NOT NULL DEFAULT '0' COMMENT '自兑换、激活之日起多少天有效';
+alter table point_exchange_code_batch modify column expiry_type TINYINT(4) NOT NULL DEFAULT '0' COMMENT '过期时间类型 0-积分过期时间，1-兑换之日起多少天有效，2-激活之日起多少天有效';
+alter table point_exchange_code modify column expiry_type TINYINT(4) NOT NULL DEFAULT '0' COMMENT '过期时间类型 0-积分过期时间，1-兑换之日起多少天有效，2-激活之日起多少天有效';
+alter table recharge_card_exchange_code_batch modify column expiry_type TINYINT(4) NOT NULL DEFAULT '0' COMMENT '过期时间类型 0-充值卡过期时间，1-兑换之日起多少天有效，2-激活之日起多少天有效';
+alter table recharge_card_exchange_code modify column expiry_type TINYINT(4) NOT NULL DEFAULT '0' COMMENT '过期时间类型 0-充值卡过期时间，1-兑换之日起多少天有效，2-激活之日起多少天有效';
+alter table voucher_exchange_code_batch modify column expiry_type TINYINT(4) NOT NULL DEFAULT '0' COMMENT '过期时间类型 0-代金券过期时间，1-兑换之日起多少天有效，2-激活之日起多少天有效';
+alter table voucher_exchange_code modify column expiry_type TINYINT(4) NOT NULL DEFAULT '0' COMMENT '过期时间类型 0-代金券过期时间，1-兑换之日起多少天有效，2-激活之日起多少天有效';
