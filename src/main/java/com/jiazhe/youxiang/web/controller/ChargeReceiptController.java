8fc5ec5bb12229f7350ea9ba1c43861413ea2f8e
giserTu <961442649@qq.com>
2019-03-18 16:06:58 +0800

    添加消费记录详情

--- src/main/java/com/jiazhe/youxiang/web/controller/ChargeReceiptController.java
+++ src/main/java/com/jiazhe/youxiang/web/controller/ChargeReceiptController.java
@@ -1,8 +1,6 @@
 package com.jiazhe.youxiang.web.controller;
 
 import com.jiazhe.youxiang.base.controller.BaseController;
-import com.jiazhe.youxiang.server.common.constant.PermissionConstant;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +18,7 @@ public class ChargeReceiptController extends BaseController {
     @RequestMapping(value = "/index")
     public String exchangeCodeIndex(String auditRecordId, Model model) {
         model.addAttribute("auditRecordId", auditRecordId);
-        return "rechargeceipt/index";
+        return "chargereceipt/index";
     }
 
 }
