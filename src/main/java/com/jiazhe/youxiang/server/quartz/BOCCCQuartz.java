4f16192cb2e9db32ff39c6a507083cb05b05b613
giserTu <961442649@qq.com>
2019-11-26 21:38:22 +0800

    正式上传商品和测试码

--- src/main/java/com/jiazhe/youxiang/server/quartz/BOCCCQuartz.java
+++ src/main/java/com/jiazhe/youxiang/server/quartz/BOCCCQuartz.java
@@ -4,6 +4,7 @@ import com.jiazhe.youxiang.base.util.boccc.AutoCouponUtils;
 import com.jiazhe.youxiang.base.util.boccc.AutoMerchantInfoUtils;
 import com.jiazhe.youxiang.base.util.boccc.AutoPicUtils;
 import com.jiazhe.youxiang.base.util.boccc.AutoProductInfoUtils;
+import com.jiazhe.youxiang.base.util.boccc.AutoSFTPUtils;
 import com.jiazhe.youxiang.base.util.boccc.BOCCCConstant;
 import com.jiazhe.youxiang.server.common.constant.EnvironmentConstant;
 import org.quartz.JobExecutionException;
@@ -107,13 +108,13 @@ public class BOCCCQuartz extends QuartzJobBean {
 //        }
 
         //定时上传指定文件夹的文件
-//        try {
-//            logger.info("定时任务：上传文件执行中");
-//            AutoSFTPUtils.upload();
-//            logger.info("定时任务：上传文件执行完成");
-//        } catch (Exception e) {
-//            logger.error("定时任务：上传文件执行失败，异常信息：" + e.getMessage());
-//        }
+        try {
+            logger.info("定时任务：上传文件执行中");
+            AutoSFTPUtils.upload();
+            logger.info("定时任务：上传文件执行完成");
+        } catch (Exception e) {
+            logger.error("定时任务：上传文件执行失败，异常信息：" + e.getMessage());
+        }
 
 //        //定时分析前一日优惠券剩余数量
 //        try {
