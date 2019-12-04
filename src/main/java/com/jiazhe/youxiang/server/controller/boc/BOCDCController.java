5d24ddabe99703bf8f06e92f7928ac85a62e1bc1
聂箫 <niexiao@maoyan.com>
2019-12-04 16:56:30 +0800

    调整uploadReconciliationFile

--- src/main/java/com/jiazhe/youxiang/server/controller/boc/BOCDCController.java
+++ src/main/java/com/jiazhe/youxiang/server/controller/boc/BOCDCController.java
@@ -24,6 +24,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -128,7 +129,7 @@ public class BOCDCController {
     @AppApi
     @ApiOperation(value = "上传对账信息", httpMethod = "GET", notes = "上传对账信息")
     @RequestMapping(value = "/uploadreconciliationfile", method = RequestMethod.GET)
-    public Object uploadReconciliationFile(int monthOffset) throws Exception {
+    public Object uploadReconciliationFile(@RequestParam("monthOffset") int monthOffset) throws Exception {
         bocdcBiz.uploadReconciliationFile(monthOffset);
         return "任务全部完成";
     }
