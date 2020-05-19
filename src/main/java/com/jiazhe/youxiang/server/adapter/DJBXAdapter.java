e41fa50d62438e7fab8b8b8a6b1a16a70cbdc1bf
DannyLittle <5696252+dannylittle@user.noreply.gitee.com>
2020-05-19 17:54:57 +0800

    修改外部登录接口

--- src/main/java/com/jiazhe/youxiang/server/adapter/DJBXAdapter.java
+++ src/main/java/com/jiazhe/youxiang/server/adapter/DJBXAdapter.java
@@ -1,6 +1,8 @@
 package com.jiazhe.youxiang.server.adapter;
 
+import com.jiazhe.youxiang.server.dto.djbx.AgentInfoDTO;
 import com.jiazhe.youxiang.server.dto.djbx.PointsQueryDTO;
+import com.jiazhe.youxiang.server.vo.resp.djbx.AgentInfoResp;
 import com.jiazhe.youxiang.server.vo.resp.djbx.PointsQueryResp;
 
 /**
@@ -18,5 +20,16 @@ public class DJBXAdapter {
         pointsQueryResp.setAgentCode(dto.getAgentCode());
         pointsQueryResp.setPoints(dto.getPoints());
         return pointsQueryResp;
+    }
+
+    public static AgentInfoResp agentInfoDTO2Resp(AgentInfoDTO agentInfoDTO) {
+        if (agentInfoDTO == null) {
+            return null;
+        }
+        AgentInfoResp agentInfoResp = new AgentInfoResp();
+        agentInfoResp.setCustomerId(agentInfoDTO.getCustomerId());
+        agentInfoResp.setAgentCode(agentInfoDTO.getAgentCode());
+        agentInfoResp.setMobile(agentInfoDTO.getMobile());
+        return agentInfoResp;
     }
 }
--- src/main/java/com/jiazhe/youxiang/server/controller/djbx/DJBXController.java
+++ src/main/java/com/jiazhe/youxiang/server/controller/djbx/DJBXController.java
@@ -5,6 +5,8 @@
  */
 package com.jiazhe.youxiang.server.controller.djbx;
 
+import com.alibaba.fastjson.JSONObject;
+import com.jiazhe.youxiang.base.util.CommonValidator;
 import com.jiazhe.youxiang.server.adapter.DJBXAdapter;
 import com.jiazhe.youxiang.server.biz.djbx.DJBXBiz;
 import com.jiazhe.youxiang.server.common.annotation.AppApi;
@@ -12,16 +14,19 @@ import com.jiazhe.youxiang.server.common.annotation.CustomLog;
 import com.jiazhe.youxiang.server.common.enums.DJBXCodeEnum;
 import com.jiazhe.youxiang.server.common.enums.LogLevelEnum;
 import com.jiazhe.youxiang.server.common.enums.ModuleEnum;
-import com.jiazhe.youxiang.server.dto.djbx.PointsQueryDTO;
 import com.jiazhe.youxiang.server.common.exceptions.DJBXException;
+import com.jiazhe.youxiang.server.dto.djbx.AgentInfoDTO;
+import com.jiazhe.youxiang.server.dto.djbx.PointsQueryDTO;
 import com.jiazhe.youxiang.server.vo.ResponseFactory;
-import com.jiazhe.youxiang.server.vo.resp.boc.BOCCCResp;
+import com.jiazhe.youxiang.server.vo.req.djbx.ExternalLoginReq;
+import com.jiazhe.youxiang.server.vo.resp.djbx.AgentInfoResp;
 import com.jiazhe.youxiang.server.vo.resp.djbx.PointsQueryResp;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,29 +52,29 @@ public class DJBXController {
     /**
      * 大家保险企业微信登录
      *
-     * @param appvalue
-     * @param code
+     * @param req
      * @return
      */
     @AppApi
-    @ApiOperation(value = "大家保险企业微信登录", response = BOCCCResp.class, notes = "大家保险企业微信登录", httpMethod = "GET")
-    @RequestMapping(value = "/externallogin", method = RequestMethod.GET)
-    @CustomLog(moduleName = ModuleEnum.DJBX, operate = "中行请求第三方退货", level = LogLevelEnum.LEVEL_3)
-    public Object externalLogin(@RequestParam("appvalue") String appvalue, @RequestParam("code") String code) {
-        LOGGER.error("HTTP调用[externalLogin]方法，appvalue:{},code:{}", appvalue, code);
-        //TODO niexiao 参数校验
-        if (!APP_VALUE.equals(appvalue)) {
+    @ApiOperation(value = "大家保险企业微信登录", httpMethod = "POST", response = AgentInfoResp.class, notes = "大家保险企业微信登录")
+    @RequestMapping(value = "/externallogin", method = RequestMethod.POST)
+    @CustomLog(moduleName = ModuleEnum.DJBX, operate = "大家保险企业微信登录", level = LogLevelEnum.LEVEL_2)
+    public Object externalLogin(@ModelAttribute ExternalLoginReq req) {
+        LOGGER.error("HTTP调用[externalLogin]方法,参数为:{}", JSONObject.toJSON(req));
+        // 参数校验
+        CommonValidator.validateNull(req);
+        if (!APP_VALUE.equals(req.getAppvalue())) {
             throw new DJBXException(DJBXCodeEnum.APPVALUE_ERROR);
         }
-        if (StringUtils.isEmpty(code)) {
+        if (StringUtils.isEmpty(req.getCode())) {
             throw new DJBXException(DJBXCodeEnum.CODE_IS_NULL);
         }
-        djbxBiz.externalLogin(appvalue, code);
-        return ResponseFactory.buildSuccess();
+        AgentInfoDTO agentInfoDTO = djbxBiz.externalLogin(req.getAppvalue(), req.getCode());
+        return ResponseFactory.buildResponse(DJBXAdapter.agentInfoDTO2Resp(agentInfoDTO));
     }
 
     @AppApi
-    @ApiOperation(value = "查询剩余积分", httpMethod = "GET",response = PointsQueryResp.class, notes = "查询剩余积分")
+    @ApiOperation(value = "查询剩余积分", httpMethod = "GET", response = PointsQueryResp.class, notes = "查询剩余积分")
     @RequestMapping(value = "/querypoints")
     @CustomLog(moduleName = ModuleEnum.DJBX, operate = "查询剩余积分", level = LogLevelEnum.LEVEL_1)
     public Object queryPoints(@RequestParam("agentCode") String agentCode) {
