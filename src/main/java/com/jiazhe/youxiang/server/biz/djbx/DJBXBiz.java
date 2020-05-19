ba1a91c33d5b99924625778f54d1abd8436a52b8
DannyLittle <5696252+dannylittle@user.noreply.gitee.com>
2020-05-19 11:32:20 +0800

    将接口地址添加到配置文件

--- src/main/java/com/jiazhe/youxiang/server/biz/djbx/DJBXBiz.java
+++ src/main/java/com/jiazhe/youxiang/server/biz/djbx/DJBXBiz.java
@@ -5,6 +5,8 @@
  */
 package com.jiazhe.youxiang.server.biz.djbx;
 
+import com.jiazhe.youxiang.base.util.HttpUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,15 +18,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class DJBXBiz {
 
+    @Value("${djbx.api.getuserinfo}")
+    private String DJBX_API_GETUSERINFO;
 
     /**
      * 企业微信外部登录
+     *
      * @param appvalue
      * @param code
      */
     public void externalLogin(String appvalue, String code) {
-
         //TODO niexiao 调用大家保险的接口，获得代理人信息
+
 
         //TODO zhaoweixin 利用代理人信息，创建自己的用户，并使该用户在登录状态
 
